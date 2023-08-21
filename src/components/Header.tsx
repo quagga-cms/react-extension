a7c6f42932619c883fd6e468c0509d25d992e6a8
Puleeno Nguyen <puleeno@gmail.com>
2023-08-21 22:32:51 +0700

    Rename asset directory

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Component } from "preact";
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Header;
+
