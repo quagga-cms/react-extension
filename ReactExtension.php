1260821fc39687f008759b3dbb945d38b0c7b597
Puleeno Nguyen <puleeno@gmail.com>
2023-09-03 17:02:42 +0700

    Update the structure for React extension

--- ReactExtension.php
+++ ReactExtension.php
@@ -2,8 +2,10 @@
 
 namespace PuleenoCMS\React;
 
+use App\Constracts\BackendExtensionConstract;
+use App\Constracts\FrontendExtensionConstract;
 use App\Core\Extension;
 
-class ReactExtension extends Extension
+class ReactExtension extends Extension implements FrontendExtensionConstract, BackendExtensionConstract
 {
 }
