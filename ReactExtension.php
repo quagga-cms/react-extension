e5fb338e42bc952b4f457c18d9cb9b8faff8117e
Puleeno Nguyen <puleeno@gmail.com>
2023-10-11 06:18:37 +0700

    Support load React on frontend

--- ReactExtension.php
+++ ReactExtension.php
@@ -8,12 +8,17 @@ use App\Constracts\BackendExtensionConstract;
 use App\Constracts\FrontendExtensionConstract;
 use App\Core\AssetManager;
 use App\Core\Assets\AssetScriptOptions;
+use App\Core\Env;
 use App\Core\Extension;
 use App\Core\Helper;
+use App\Core\HookManager;
+use PuleenoCMS\Layout\TemplateManager;
 
 class ReactExtension extends Extension implements FrontendExtensionConstract, BackendExtensionConstract
 {
     protected static $react;
+
+    const REACT_FRONTEND_TEMPLATE = 'app.twig';
 
     public function getReactAsset(): AssetConstract {
         if (is_null(static::$react)) {
@@ -39,5 +44,14 @@ class ReactExtension extends Extension implements FrontendExtensionConstract, Ba
     }
 
     public function run() {
+        if (Env::get('REACT_FRONTEND', false)) {
+            $view = TemplateManager::getView();
+            if (!is_null($view)) {
+                $view->addPath(implode(DIRECTORY_SEPARATOR, [$this->getExtensionDir(), 'resources', 'views']));
+            }
+            HookManager::addFilter('home_template', function($template){
+                return static::REACT_FRONTEND_TEMPLATE;
+            });
+        }
     }
 }
